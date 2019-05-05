31db7535d00ac4db98505729544e1c4a9e79c893
Thái Đỗ <thai1998.02.06@gmail.com>
2019-05-06 02:57:31 +0700

    Add slide template

--- src/demo.py
+++ src/demo.py
@@ -18,8 +18,8 @@ def draw_graph(matrix, m = 20, n = 20):
 	cax = div.append_axes('right', '5%', '5%')
 	ax.cla()
 	cax.cla()
-	ax.set_xlabel("$x$ Position", labelpad=15, size=18)
-	ax.set_ylabel("$y$ Position", labelpad=15, size=18)
+	ax.set_xlabel("$l$", labelpad=15, size=18)
+	ax.set_ylabel("$m$", labelpad=15, size=18)
 	ax.set_aspect('equal')
 	xgrid = np.linspace(0, 1, m)
 	ygrid = np.linspace(0, 1, n)
@@ -27,7 +27,7 @@ def draw_graph(matrix, m = 20, n = 20):
 	Z = matrix
 	sol = ax.contourf(X, Y, Z, cmap= "plasma")
 	clb = fig.colorbar(sol, cax=cax, spacing='proportional')
-	clb.ax.set_ylabel("Density", labelpad=15, size=18)
+	clb.ax.set_ylabel("Concentration", labelpad=15, size=18)
 	plt.show()
 	
 def main():
@@ -37,13 +37,10 @@ def main():
 	matrix = [ [ 0.0 for i in range(n) ] for j in range(m) ]
 	for i in range(m):
 		for j in range(n):
-			print ('i = ', i)
-			print ('j = ', j)
 			if i >= (m/2-5) and i < (m/2+5) and j >= (n/2-5) and j < (n/2+5):
 				matrix[i][j] = 80.0
 			else:
 				matrix[i][j] = 25.0
-	print (matrix)
 	draw_graph(matrix)
 	
 	#After
