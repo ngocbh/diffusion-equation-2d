84a39f5aaa5484b494acbb2a642bfccadbe3a61f
Thái Đỗ <thai1998.02.06@gmail.com>
2019-05-13 18:12:59 +0700

    Fix some files and videos

--- src/demo.py
+++ src/demo.py
@@ -1,51 +1,64 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.animation as animation
+import sys
 
-def file_to_matrix(path = 'out.txt'):
+def file_to_matrix(path = 'out.txt', m = 20):
+	list_of_matrix = []
 	matrix = []
+	i = 0
 	with open(path) as f:
 		for line in f:
+			i += 1
 			submatrix = [float(x) for x in line.split()]
 			matrix.append(submatrix)
-	return matrix
+			if i % m == 0:
+				list_of_matrix.append(matrix)
+				matrix = []
+		list_of_matrix.append(matrix)
+	return list_of_matrix
 
-def draw_graph(matrix, m = 20, n = 20):
-	# Create figure
+def make_video(list_of_matrix, m = 20, n = 20, name = 'demo.mp4'):
 	fig = plt.figure(figsize=[12,8])
 	ax = fig.add_subplot(111)
 	div = make_axes_locatable(ax)
 	cax = div.append_axes('right', '5%', '5%')
-	ax.cla()
-	cax.cla()
-	ax.set_xlabel("$l$", labelpad=15, size=18)
-	ax.set_ylabel("$m$", labelpad=15, size=18)
-	ax.set_aspect('equal')
-	xgrid = np.linspace(0, 1, m)
-	ygrid = np.linspace(0, 1, n)
-	X, Y = np.meshgrid(xgrid, ygrid)
-	Z = matrix
-	sol = ax.contourf(X, Y, Z, cmap= "plasma")
-	clb = fig.colorbar(sol, cax=cax, spacing='proportional')
-	clb.ax.set_ylabel("Concentration", labelpad=15, size=18)
-	plt.show()
-	
+	nFrames = len(list_of_matrix) - 1
+	def draw_graph(i, list_of_matrix, m, n):
+		xgrid = np.linspace(0, 1, m)
+		ygrid = np.linspace(0, 1, n)
+		X, Y = np.meshgrid(xgrid, ygrid)
+		Z = list_of_matrix[i]
+		sol = ax.contourf(X, Y, Z, 1000, cmap= "plasma")
+		if i < 51:
+			ax.set_title("n = {}".format(i))
+		elif i != nFrames - 1:
+			ax.set_title("n = {}".format(20*(i-50)))
+		else:
+			ax.set_title("n = {}".format(list_of_matrix[i+1][0][0]))
+		ax.set_xlabel("$l$", labelpad=15, size=18)
+		ax.set_ylabel("$m$", labelpad=15, size=18)
+		ax.set_aspect('equal')
+		ticks = np.linspace(25.0, 80.0, num=11)
+		clb = fig.colorbar(sol, cax=cax, ticks = ticks, spacing='proportional')
+		clb.ax.set_ylabel("Concentration", labelpad=15, size=18)
+		if i == 0:
+			plt.savefig('Before100')
+		if i == nFrames - 1:
+			plt.savefig('After100')
+			sys.exit(0)
+	ani = animation.FuncAnimation(fig, draw_graph, fargs=(list_of_matrix,m,n), blit=False,
+                              repeat=False, save_count=0, frames=nFrames)
+	ani.save(name, writer='ffmpeg', dpi=180, fps=5)
+
 def main():
-	#Before
-	m = 20
-	n = 20
-	matrix = [ [ 0.0 for i in range(n) ] for j in range(m) ]
-	for i in range(m):
-		for j in range(n):
-			if i >= (m/2-5) and i < (m/2+5) and j >= (n/2-5) and j < (n/2+5):
-				matrix[i][j] = 80.0
-			else:
-				matrix[i][j] = 25.0
-	draw_graph(matrix)
-	
 	#After
-	matrix = file_to_matrix()
-	draw_graph(matrix)
-	
+	m = 100
+	n = 100
+	fig = plt.figure()
+	list_of_matrix = file_to_matrix('out.txt',m)
+	print(len(list_of_matrix))
+	make_video(list_of_matrix, m, n, 'demo100_3.mp4')
 if __name__ == '__main__':
 	main()
--- src/gauss_seidel_seq.cpp
+++ src/gauss_seidel_seq.cpp
@@ -1,7 +1,7 @@
 /*
  *  Problem : main.c
- *  Description : 
- *  Created by ngocjr7 on 
+ *  Description :
+ *  Created by ngocjr7 on
 */
 
 #include <bits/stdc++.h>
@@ -13,8 +13,8 @@ const long N = 100000 + 7;
 const long INF = 1000000000 + 7;
 const long MODULE = 1000000000 + 7;
 
-#define m 20
-#define n 20
+#define m 100
+#define n 100
 #define epsilon 0.001
 #define tolerance 0.001
 
@@ -38,10 +38,10 @@ void KhoiTao(float *C)
 {
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < n; j++)
-			if ( i >= (m/2-5) && i < (m/2+5) && j >= (n/2-5) && j < (n/2+5) ) 
+			if ( i >= (m/2-5) && i < (m/2+5) && j >= (n/2-5) && j < (n/2+5) )
 				*(C+i*n+j) = 80.0;
-			else 
-				*(C+i*n+j) = 25.0; 
+			else
+				*(C+i*n+j) = 25.0;
 }
 
 void Gauss_Seidel(float *C)
@@ -50,11 +50,12 @@ void Gauss_Seidel(float *C)
 	float delta = 0;
 	float *Cn;
 	Cn = (float*)malloc(n*m*sizeof(float));
+    int cnt = 0, gap = 0, loop = 0, printed = 0;
 
 	do {
 		delta = 0;
 		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++) 
+			for (int j = 0; j < n; j++)
 				if ( (i+j)%2 == 0 ) {
 					if (i >= (m/2-5) && i < (m/2+5) && j >= (n/2-5) && j < (n/2+5)) {
 						*(Cn+i*n+j) = 80.0;
@@ -66,7 +67,7 @@ void Gauss_Seidel(float *C)
 						*(Cn+i*n+j) = 0.25 * (*(C+(i-1)*n+j) + *(C+(i+1)*n+j) + *(C+i*n+j-1) + *(C+i*n+j+1));
 					}
 
-					if ( abs(*(Cn+i*n+j) - *(C+i*n+j)) > tolerance ) 
+					if ( abs(*(Cn+i*n+j) - *(C+i*n+j)) > tolerance )
 						delta = abs(*(Cn+i*n+j) - *(C+i*n+j));
 				} else {
 					*(Cn+i*n+j) = *(C+i*n+j);
@@ -75,7 +76,7 @@ void Gauss_Seidel(float *C)
 		copy_array(C,Cn);
 
 		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++) 
+			for (int j = 0; j < n; j++)
 				if ( (i+j)%2 == 1 ) {
 					if (i >= (m/2-5) && i < (m/2+5) && j >= (n/2-5) && j < (n/2+5)) {
 						*(Cn+i*n+j) = 80.0;
@@ -87,27 +88,36 @@ void Gauss_Seidel(float *C)
 						*(Cn+i*n+j) = 0.25 * (*(C+(i-1)*n+j) + *(C+(i+1)*n+j) + *(C+i*n+j-1) + *(C+i*n+j+1));
 					}
 
-					if ( abs(*(Cn+i*n+j) - *(C+i*n+j)) > tolerance ) 
+					if ( abs(*(Cn+i*n+j) - *(C+i*n+j)) > tolerance )
 						delta = abs(*(Cn+i*n+j) - *(C+i*n+j));
 				} else {
 					*(Cn+i*n+j) = *(C+i*n+j);
 				}
 
 		copy_array(C,Cn);
-		// print_array(C);
+        if ( printed < 50 ) {
+            print_array(C);
+        } else {
+			if (printed % 20 == 0) {
+				print_array(C);
+			}
+		}
+		++printed;
 		// cout << "-----------------------------" << endl << delta << endl << "---------------------------" << endl;
-	} while (delta > tolerance); 
+	} while (delta > tolerance);
 
 	print_array(C);
+	cout << printed;
 }
 
 int main()
 {
 	freopen("in.txt","r",stdin);
-
+	freopen("out.txt","w",stdout);
 	float *C;
 	C = (float*)malloc(m*n*sizeof(float));
 	KhoiTao(C);
+	print_array(C);
 	Gauss_Seidel(C);
 
 	return 0;
